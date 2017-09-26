ad258ae5c1f5a8ac4467fb83b5045a76093585b4
Ryan Cavanaugh <RyanCavanaugh@users.noreply.github.com>
2017-09-26 15:23:57 -0700

    Add an inline template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,19 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app',
+  template: '<span>{{prop1.tolower}}</spa>',
+})
+export class InlineComponent {
+  prop1 = 'hello';
+  prop2 = 'world';
+}
+
+@Component({
+  selector: 'app',
   templateUrl: './app.component.ngml',
 })
 export class AppComponent {
   prop1 = 'hello';
   prop2 = 'world';
 }
+
